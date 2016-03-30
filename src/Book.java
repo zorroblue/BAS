a6477e47a907d69e9d97bd31abf2551a05dd8167
Aditya Bhagwat <ewsobourgeois@gmail.com>
2016-03-30 18:55:00 +0530

    Basic interface done

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Book {
+	private String bookTitle;
+	private String authorName;
+	private String ISBN;
+	private int price;
+	private int noOfCopies;
+	private int noOfRequests;
+	private Publisher publisher;
+	private int inventoryLevel;
+	private File imageFile;
+
+}
